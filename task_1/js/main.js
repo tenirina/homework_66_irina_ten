06128ec4b1efa98625842d687eedcc238251f8be
Irina Ten <virhot17@gmail.com>
2022-11-08 08:48:29 +0600

    Add
    Реализован вывод в консоль скороговорки

---
+++ task_1/js/main.js
@@ -0,0 +1,7 @@
+let divs = document.getElementsByTagName('div');
+console.log(divs[2].innerText);
+console.log(divs[4].innerText);
+console.log(divs[1].innerText);
+console.log(divs[5].innerText);
+console.log(divs[3].innerText);
+console.log(divs[0].innerText);
