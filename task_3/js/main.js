013617bbd6358763b84e89cbff0cb4150128fd6f
Irina Ten <virhot17@gmail.com>
2022-11-08 10:08:55 +0600

    Improvemente
    Индексы элементов окрашиваются рандомно в третьем задании.

--- task_3/js/main.js
+++ task_3/js/main.js
@@ -1,7 +1,13 @@
 const containers = document.getElementsByClassName('container')[0]
+
+
 for(let i=0; i<5; i++){
-    const newElement = document.createElement('div')
-    newElement.classList.add('element')
-    newElement.innerText = 'Element ' + [i+1]
-    containers.append(newElement)
+    const newElement = document.createElement('div');
+    newElement.classList.add('element');
+    const newIndex = document.createElement('span');
+    newIndex.style.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+    newElement.innerText = 'Element ';
+    newIndex.innerText = i+1;
+    newElement.append(newIndex);
+    containers.append(newElement);
 }
